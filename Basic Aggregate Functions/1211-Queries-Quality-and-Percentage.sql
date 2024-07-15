f99c134b9a24e2e708379f5bbeeb92d7ccbd0c9c
Hend Ahmed <92755637+LifelongLearner-HEND@users.noreply.github.com>
2024-07-15 21:52:40 +0300

    [1211] [Time Beats: 66.42%] - LeetPush

---
+++ Basic Aggregate Functions/1211-Queries-Quality-and-Percentage.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT query_name, 
+ROUND(SUM(rating/position)/COUNT(*), 2) AS quality, 
+ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)/COUNT(*) * 100 ,2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
+HAVING query_name IS NOT NULL
